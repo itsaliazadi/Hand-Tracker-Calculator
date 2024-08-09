108ae789e24ef44cf24298ffeb1b49ee3d6306f5
Ali Azadi <aliazadicode@gmail.com>
2024-08-09 16:51:04 +0330

    Added a velocity variable to control the sensitivity and Fixed the reversed reflection problem

--- NumberDetection.py
+++ NumberDetection.py
@@ -1,7 +1,7 @@
 import cv2
 import Gui
 import time
-from cvzone.HandTrackingModule import HandDetector 
+from cvzone.HandTrackingModule import HandDetector  # type: ignore
 
 detector = HandDetector(maxHands=2, detectionCon=0.8)
 
@@ -23,12 +23,25 @@ def getThumbTip(hand):
     thumbTip = hand['lmList'][4]
     return thumbTip
 
+preIndexTip = (0, 0)
+clickDelay = 0.5
+lastClickTime = 0
+
 def isClicking(indexTip, thumbTip, button):
+    global preIndexTip, lastClickTime
+
     xi, yi, _ = [int(i) for i in indexTip]
     xt, yt, _ = [int(j) for j in thumbTip]
 
+    avg_x = int((xi + preIndexTip[0]) / 2)
+    avg_y = int((yi + preIndexTip[1]) / 2)
+
+    preIndexTip = (avg_x, avg_y)
+
     distance = ((xi-xt)**2 + (yi-yt)**2) ** 0.5
-    if distance < 20 and button['x'] < xi < button['x'] + 60 and button['y'] < yi < button['y'] + 50:
+    velocity = distance / (time.time() - lastClickTime)
+    if distance < 20 and button['x'] <= xi <= button['x'] + 70 and button['y'] <= yi <= button['y'] + 60 and velocity < 200:
+        lastClickTime = time.time()
         return True
     return False
 
@@ -38,6 +51,7 @@ cv2.setMouseCallback('Image', mouse_callback)
 
 while True:
     success, img = video.read()
+    img = cv2.flip(img, 1)
     hands, img = detector.findHands(img)
 
     calculator.drawCalculator(img)
@@ -55,6 +69,7 @@ while True:
                 if isClicking(indexTip, thumbTip, button):
                     print(button['text'])
                     calculator.handleClick(button['x'], button['y'])
+                    time.sleep(0.1)
                     break
 
 
