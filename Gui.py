499c53b2cb7f97ffc9d704b70c48157439c68fdf
Ali Azadi <aliazadicode@gmail.com>
2024-08-13 15:08:39 +0330

    Added the delete button and removed the magic numbers from the code

--- Gui.py
+++ Gui.py
@@ -23,20 +23,34 @@ class Calculator:
             {'text': '.', 'x': 120, 'y': 260},
             {'text': '=', 'x': 210, 'y': 260},
             {'text': '+', 'x': 300, 'y': 260},
+            {'text': 'delete', 'x': 30, 'y': 330, 'width': 100, 'height': 30}
         ]
     
     def drawCalculator(self, frame):
         # Drawing the background
-        cv2.rectangle(frame, (0, 0), (400, 350), (112, 19, 19), -1)
+        calculatorWidth = 400
+        calculatorHeight = 370
+        calculatorColor = (112, 19, 19)
+        cv2.rectangle(frame, (0, 0), (calculatorWidth, calculatorHeight), calculatorColor, -1)
 
         # Displaying the number
         cv2.rectangle(frame, (20, 20), (360, 40), (255, 255, 255), -1)
         cv2.putText(frame, self.displayValue, (30, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1)
 
+        whiteButton = (200, 200, 200)
+        redButton = (31, 24, 128)
+
         for button in self.buttons:
-            cv2.rectangle(frame, (button['x'], button['y']), (button['x'] + 70, button['y'] + 50), (200, 200, 200), -1)
-            cv2.putText(frame, button['text'], (button['x'] + 30, button['y'] + 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
-    
+            if button['text'] != "delete":
+                if button['text'] in ('=', '+', '-', '/', '*'):
+                    cv2.rectangle(frame, (button['x'], button['y']), (button['x'] + 70, button['y'] + 50), redButton, -1)
+                    cv2.putText(frame, button['text'], (button['x'] + 30, button['y'] + 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
+                else:
+                    cv2.rectangle(frame, (button['x'], button['y']), (button['x'] + 70, button['y'] + 50), whiteButton, -1)
+                    cv2.putText(frame, button['text'], (button['x'] + 30, button['y'] + 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
+            else:
+                cv2.rectangle(frame, (button['x'], button['y']), (button['x'] + button['width'], button['y'] + button['height']), (31, 24, 128), -1)
+                cv2.putText(frame, button['text'], (button['x'] + 8, button['y'] + 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
 
     def handleClick(self, x, y):
         for button in self.buttons:
@@ -46,6 +60,11 @@ class Calculator:
                         self.displayValue = str(eval(self.displayValue))
                     except Exception as e:
                         self.displayValue = ""
+                elif button['text'] == "delete":
+                    try:
+                        self.displayValue = self.displayValue[:-1]
+                    except Exception as e:
+                        pass
                 else:
                     if self.displayValue == "0" and button['text'] != '.':
                         self.displayValue = button['text']
